7e421e6a4be5f0461f02277943503c41fcfbca18
Tho <Tho@example.com>
2021-10-07 22:47:55 +0700

    some translation

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -78,7 +78,7 @@ function onMessageReceived(payload) {
 
     if(message.type === 'JOIN') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' joined!';
+        message.content = message.sender + ' đã tham gia';
     } else if (message.type === 'LEAVE') {
         messageElement.classList.add('event-message');
         message.content = message.sender + ' left!';
