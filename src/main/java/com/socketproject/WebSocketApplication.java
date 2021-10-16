1458dbdd310f835cb1a3827768330a500c1d2c27
tholv2000 <45034846+tholv2000@users.noreply.github.com>
2021-10-16 18:09:58 +0700

    Update WebSocketApplication.java

--- src/main/java/com/socketproject/WebSocketApplication.java
+++ src/main/java/com/socketproject/WebSocketApplication.java
@@ -13,15 +13,5 @@ public class WebSocketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebSocketApplication.class, args);
 	}
-	
-	@Bean
-        public WebMvcConfigurer corsConfigurer() {
-	    return new WebMvcConfigurer() {
-	        @Override
-	        public void addCorsMappings(CorsRegistry registry) {
-		    registry.addMapping("**").allowedOrigins("*").allowedMethods("*");
-	        }
-	    };
-        }
 
 }
