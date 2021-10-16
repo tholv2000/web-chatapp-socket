fb5896bcd0ab1e93e46f114e93d5d36f5717379d
tholv2000 <45034846+tholv2000@users.noreply.github.com>
2021-10-16 16:00:02 +0700

    try add cors again

--- src/main/java/com/socketproject/WebSocketApplication.java
+++ src/main/java/com/socketproject/WebSocketApplication.java
@@ -9,5 +9,15 @@ public class WebSocketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebSocketApplication.class, args);
 	}
+	
+	@Bean
+        public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+		    registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+	        }
+	    };
+        }
 
 }
