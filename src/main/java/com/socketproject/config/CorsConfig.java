572958556c159303766f3cabe947639761e9d0bf
tholv2000 <45034846+tholv2000@users.noreply.github.com>
2021-10-16 17:11:34 +0700

    Update CorsConfig.java

--- src/main/java/com/socketproject/config/CorsConfig.java
+++ src/main/java/com/socketproject/config/CorsConfig.java
@@ -12,6 +12,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.allowedOrigins("*").allowedMethods("*");
+        registry.addMapping("**").allowedOrigins("*").allowedMethods("*");
     }
 }
