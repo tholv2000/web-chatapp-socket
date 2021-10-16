42a4befb42d8070326a827fd92c0855800de38e7
tholv2000 <45034846+tholv2000@users.noreply.github.com>
2021-10-16 17:16:00 +0700

    Update WebSocketApplication.java

--- src/main/java/com/socketproject/WebSocketApplication.java
+++ src/main/java/com/socketproject/WebSocketApplication.java
@@ -19,7 +19,7 @@ public class WebSocketApplication {
 	    return new WebMvcConfigurer() {
 	        @Override
 	        public void addCorsMappings(CorsRegistry registry) {
-		    registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+		    registry.addMapping("**").allowedOrigins("*").allowedMethods("*");
 	        }
 	    };
         }
