fa6b6794c233e892d3def44e343b2a3cbb550bb9
tholv2000 <45034846+tholv2000@users.noreply.github.com>
2021-10-16 17:25:47 +0700

    Update CorsConfig.java

--- src/main/java/com/socketproject/config/CorsConfig.java
+++ src/main/java/com/socketproject/config/CorsConfig.java
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("").allowedOrigins("*").allowedMethods("*");
-//     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    }
 }
